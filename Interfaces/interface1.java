2f343ab5f6340213b29766a26320b9175e0247fa
Vamsikeela <vamsikeela07@gmail.com>
2025-03-07 19:07:57 +0530

    Add files via upload

---
+++ Interfaces/interface1.java
@@ -0,0 +1,16 @@
+package Interfaces;
+
+interface MyInterface {
+    void show();
+}
+
+class MyClass implements MyInterface {
+    public void show() {
+        System.out.println("Implemented method in MyClass.");
+    }
+
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.show();
+    }
+}
